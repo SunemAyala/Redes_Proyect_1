219d771c5e7b029f0f0bea5348f61541757f1c52
Agustin Flores Lopez <afl231001@gmail.com>
2026-05-15 13:30:31 -0600

    se agrego un ejemplo de.env y se modifico el settings.py para usar las variables definidas en .env

--- config/settings.py
+++ config/settings.py
@@ -11,22 +11,23 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = os.getenv("DEBUG") == "True"
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-alhti1a(lzu2we-1ojm7v%3i01w7r8ekejbz5v16p@0gx5no67"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
